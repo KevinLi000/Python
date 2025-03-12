162f388461b553838ef4999e1d715f81e7cb4df0
Kevin Li <kevin.li@greendotcorp.com>
2025-03-12 15:59:51 +0800

    Update code

--- common.py
+++ common.py
@@ -1,4 +1,26 @@
 from github import Github
+import subprocess
 #generate github by access token
 def generateGitHubByAccessToken(accessToken):
     return Github(accessToken)
+def run_command(command):
+    """Excute shell command and return re"""
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    if result.returncode != 0:
+        print(f"Error: {result.stderr}")
+    return result.stdout.strip()
+"""
+# branch_name: the branch name
+"""
+def push_branch(branch_name, remote="origin"):
+    """Commit the current changes and push it to remote branch"""
+    # 添加所有文件
+    run_command("git add .")
+    
+    # 提交更改
+    commit_message = f"Auto commit changes to {branch_name}"
+    run_command(f'git commit -m "{commit_message}"')
+    
+    # 推送到远程
+    run_command(f"git push {remote} {branch_name}")
+    print(f"Branch {branch_name} pushed to {remote}")
---
+++ repositoryhelper.py
@@ -0,0 +1,37 @@
+from github import GithubException
+import subprocess
+
+class RepositoryHelper:
+    def __init__(self, github, repoName):
+       self.github = github
+       self.repoName = repoName
+    def getRepository(self, repoName):
+        try:
+            repo = self.github.get_repo(repoName)
+            return repo
+        except GithubException as e:
+            print(e)
+            if e.status == 404:
+                user = self.github.get_user()
+                print(user)
+                # repo does not exist, create it
+                repo = user.create_repo(repoName)
+                return repo
+            else:
+                # Other excetions
+                return
+    def switch_or_create_branch(self,repo, branch_name, remote_name='origin'):
+        # Check whether the branch exists
+        branches = [branch.name for branch in repo.get_branches()]
+        for branch in branches:
+            print(branch.strip())
+        if branch_name in branches:
+            print(f"分支 '{branch_name}' 已存在，切换到该分支。")
+            repo.checkout(branch_name)
+        else:
+            print(f"分支 '{branch_name}' 不存在，创建新分支。")
+            repo.checkout('-b', branch_name)
+
+        # 推送分支到远端
+        repo.push(remote_name, branch_name, set_upstream=True)
+        print(f"分支 '{branch_name}' 已成功推送到远程 '{remote_name}'。")
